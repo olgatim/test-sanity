9bdaeafb616ace26d2d9b23652601c1f1ec6cbf1
olga_tim <tigercub220@gmail.com>
2020-07-20 23:15:19 +0300

    Modify index

--- web/src/pages/index.js
+++ web/src/pages/index.js
@@ -1,22 +1,39 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const {
+    allSanityBlogPost: { edges },
+  } = useStaticQuery(graphql`
+    query {
+      allSanityBlogPost {
+        totalCount
+        edges {
+          node {
+            _id
+            name
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div>
+        {edges.map(({ node }) => {
+          return <div key={node._id}>{node.name}</div>
+        })}
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
